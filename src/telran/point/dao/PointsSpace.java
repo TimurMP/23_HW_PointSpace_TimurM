15acc955b205b2f7567d462c723859107416ff1a
TimurMP <titimurmamyshev@gmail.commur>
2022-06-27 19:03:27 +0300

    Done with Mistakes

--- src/telran/point/dao/PointsSpace.java
+++ src/telran/point/dao/PointsSpace.java
@@ -10,17 +10,16 @@ public class PointsSpace {
     Point[] points;
     Comparator<Point> comp = (p1, p2) -> {
         Point compPoint = relPoint;
-        double distanceXP1 = ((p1.getX()- compPoint.getX()));
-        double distanceYP1 = ((p1.getY()- compPoint.getY()));
-        double distanceP1 = Math.sqrt(distanceXP1*distanceXP1)+(distanceYP1*distanceYP1);
+        double distanceXP1 = ((p1.getX() - compPoint.getX()));
+        double distanceYP1 = ((p1.getY() - compPoint.getY()));
+        double distanceP1 = Math.sqrt(distanceXP1 * distanceXP1) + (distanceYP1 * distanceYP1);
 
-        double distanceXP2 = ((p2.getX()- compPoint.getX()));
-        double distanceYP2 = ((p2.getY())-compPoint.getY());
-        double distanceP2 = Math.sqrt(distanceXP2*distanceXP2)+(distanceYP2*distanceYP2);
+        double distanceXP2 = ((p2.getX() - compPoint.getX()));
+        double distanceYP2 = ((p2.getY()) - compPoint.getY());
+        double distanceP2 = Math.sqrt(distanceXP2 * distanceXP2) + (distanceYP2 * distanceYP2);
 
         return Double.compare(distanceP1, distanceP2);
     };
-
 
 
     public PointsSpace(Point relPoint, Point[] points) {
@@ -40,19 +39,19 @@ public class PointsSpace {
         return points;
     }
 
-    public  void  addPoint(Point point){
+    public void addPoint(Point point) {
         printArray();
         int index = Arrays.binarySearch(points, point, comp);
         System.out.println(index);
-        Point[] pointCopy = new Point[points.length+1];
-        if (index<0){
-            System.arraycopy(points, -index-1, pointCopy, -index, points.length+index+1);
-            System.arraycopy(points, index-index, pointCopy, index-index, 1);
-            pointCopy[-index-1] = point;
+        Point[] pointCopy = new Point[points.length + 1];
+        if (index < 0) {
+            System.arraycopy(points, -index - 1, pointCopy, -index, points.length + index + 1);
+            System.arraycopy(points, index - index, pointCopy, index - index, index - index + 1);
+            pointCopy[-index - 1] = point;
         }
-        if (index>0){
-            System.arraycopy(points, index, pointCopy, index+1, points.length-index);
-            System.arraycopy(points, index-index, pointCopy, index-index, index);
+        if (index > 0) {
+            System.arraycopy(points, index, pointCopy, index + 1, points.length - index);
+            System.arraycopy(points, index - index, pointCopy, index - index, index);
             pointCopy[index] = point;
         }
         points = pointCopy;
@@ -62,9 +61,6 @@ public class PointsSpace {
         }
 
 
-
-
-
         //TODO
         //Maintain sorting state (Without re-sorting again). Apply binarySearch from Arrays.
         //to apply method arrayCopy from System.
@@ -72,7 +68,7 @@ public class PointsSpace {
     }
 
 
-    public void printArray(){
+    public void printArray() {
         System.out.println("========================");
         for (int i = 0; i < points.length; i++) {
             System.out.println(points[i]);
--- src/telran/point/tests/PointsSpaceTest.java
+++ src/telran/point/tests/PointsSpaceTest.java
@@ -18,14 +18,13 @@ class PointsSpaceTest {
     Point p5 = new Point(5, 5);
     Point p6 = new Point(6, 6);
     Point p_2 = new Point(-2, -2);
-    Point[] expectedTemp = { p_2, p1, p2, p3, p4 };
-    Point[] original = { p3, p2, p0, p4 };
-    Point[] expected0 = { p0, p2, p3, p4 };
-    Point[] expected4 = { p4, p3, p2, p0 };
-    Point[] expected01 = { p0, p1, p2, p3, p4 };
-    Point[] expected01_02 = { p0, p1, p_2, p2, p3, p4 };
-
-
+    Point[] expectedTemp = {p_2, p1, p2, p3, p4};
+    Point[] original = {p3, p2, p0, p4};
+    Point[] expected0 = {p0, p2, p3, p4};
+    Point[] expected4 = {p4, p3, p2, p0};
+    Point[] expected01 = {p0, p1, p2, p3, p4};
+    Point[] expected05 = {p0, p2, p3, p4, p5};
+    Point[] expected01_02 = {p0, p1, p_2, p2, p3, p4};
 
 
     @Test
@@ -54,28 +53,33 @@ class PointsSpaceTest {
         Point relPoint = p4;
         Comparator<Point> comp = (p1, p2) -> {
 
-            double distanceXP1 = ((p1.getX()- relPoint.getX()));
-            double distanceYP1 = ((p1.getY()- relPoint.getY()));
-            double distanceP1 = Math.sqrt(distanceXP1*distanceXP1)+(distanceYP1*distanceYP1);
+            double distanceXP1 = ((p1.getX() - relPoint.getX()));
+            double distanceYP1 = ((p1.getY() - relPoint.getY()));
+            double distanceP1 = Math.sqrt(distanceXP1 * distanceXP1) + (distanceYP1 * distanceYP1);
 
-            double distanceXP2 = ((p2.getX()- relPoint.getX()));
-            double distanceYP2 = ((p2.getY())-relPoint.getY());
-            double distanceP2 = Math.sqrt(distanceXP2*distanceXP2)+(distanceYP2*distanceYP2);
+            double distanceXP2 = ((p2.getX() - relPoint.getX()));
+            double distanceYP2 = ((p2.getY()) - relPoint.getY());
+            double distanceP2 = Math.sqrt(distanceXP2 * distanceXP2) + (distanceYP2 * distanceYP2);
 
             return Double.compare(distanceP1, distanceP2);
         };
 
 
-        Point[] original = { p3, p2, p0, p4 };
+        Point[] original = {p3, p2, p0, p4};
         printArray(original, "Original");
         Point[] copy = Arrays.copyOf(original, original.length);
         Arrays.sort(copy, comp);
         printArray(copy, "copy");
+    }
 
 
-
-
-
+    @Test
+    void testAddPoint1() {
+        PointsSpace ps = new PointsSpace(p0, original);
+        ps.addPoint(p5);
+        assertArrayEquals(expected05, ps.getPoints());
+//        ps.addPoint(p_2);
+//        assertArrayEquals(expected01_02, ps.getPoints());
     }
 
     private void printArray(Point[] arr, String title) {
